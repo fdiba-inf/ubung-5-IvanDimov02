7b441438220fdb94ca31b8d0c74a47e6e3b9dea4
IvanDimov02 <>
2021-11-17 22:26:49 +0000

    Some message

--- src/exercise5/Search.java
+++ src/exercise5/Search.java
@@ -10,17 +10,28 @@ public class Search {
         int numberCount = input.nextInt();
 
         int[] numbers = new int[numberCount];
+        
+    
+        
 
         System.out.println("Enter numbers: ");
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = input.nextInt();
+        
         }
 
         System.out.print("Search for number: ");
         int number = input.nextInt();
-
+        
         int numberIndex = -1;
-        // Search for number in numbers
+
+        for( int index = 0; index < numbers.length; index++) {
+          if(numbers[index] == number) {
+            numberIndex = index;
+          }
+        }
+        
+
 
         System.out.println("Number index: " + numberIndex);
     }
--- src/exercise5/UniqueNumbers.java
+++ src/exercise5/UniqueNumbers.java
@@ -11,14 +11,28 @@ public class UniqueNumbers {
         int numberCount = input.nextInt();
 
         int[] numbers = new int[numberCount];
+        
 
         System.out.println("Enter numbers: ");
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = input.nextInt();
         }
+        
 
         int[] uniqueNumbers = new int[numbers.length];
-        // Find unique numbers in numbers
+        
+        uniqueNumbers[0] = numbers[0];
+        for(int index = 1; index < numberCount; index++) {
+          for(int j = 0; j < numberCount; j++) {
+            if(uniqueNumbers[j] == numbers[index]) {
+              break;
+            } else if (uniqueNumbers[j]== 0) {
+              uniqueNumbers[j] = numbers[index];
+              break;
+            }
+
+          }
+        }
 
         String uniqueNumbersAsString = Arrays.toString(uniqueNumbers);
         System.out.println("Unique numbers: " + uniqueNumbersAsString);
